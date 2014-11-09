5b251770c78847b1b5ec005f1b7d1d1145b3e95d
Bruce LANE <bruce@batchass.fr>
2014-11-09 16:48:43 +0100

    node.js

---
+++ Batchass/routes/index.js
@@ -0,0 +1,16 @@
+﻿
+/*
+ * GET home page.
+ */
+
+exports.index = function (req, res) {
+    res.render('index', { title: 'Batchass', year: new Date().getFullYear() });
+};
+
+exports.about = function (req, res) {
+    res.render('about', { title: 'About', year: new Date().getFullYear(), message: 'Adapted from cabbi.bo.' });
+};
+
+exports.contact = function (req, res) {
+    res.render('contact', { title: 'Contact', year: new Date().getFullYear(), message: 'Batchass is available for AV performances.' });
+};
